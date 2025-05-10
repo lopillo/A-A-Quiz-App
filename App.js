43f8f4a8c57e880c4f989d9a7323fad52d11051c
lopillo <carlosjavierlopezgonzalez@gmail.com>
2025-05-10 23:02:20 +0200

    Cleaned up app folder and created App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+// App.js
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Welcome to A&A Quiz App!</Text>
+    </View>
+  );
+}
